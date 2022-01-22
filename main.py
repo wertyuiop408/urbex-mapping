bfa392bcb611ea3a857f2cf6561b20b79d64343a
wertyuiop408 <wertyuiop408@users.noreply.github.com>
2022-01-22 10:44:54 +0000

    Added date of the thread to the refs commits now occur for each section page rather than after entire section crawled http status code included in debug message

--- main.py
+++ main.py
@@ -62,13 +62,46 @@ def createTables() -> None:
         url TEXT,
         title TEXT,
         place_id INTEGER,
-        date_inserted TEXT,
-        date_scrape TEXT,
+        date_inserted TEXT, /* date we inserted the entry into the db*/
+        date_scrape TEXT, /* date that the full thread was scraped */
+        date_post TEXT, /* date that a thread was posted */
         raw TEXT,
         UNIQUE("url")
         )""")
 
     #create a table to handle changes and modifications?
+    return
+
+
+def get_2dl() -> None:
+    """
+        https://www.28dayslater.co.uk/forum/noteworthy-reports.115/
+        https://www.28dayslater.co.uk/forum/asylums-and-hospitals.4/
+        https://www.28dayslater.co.uk/forum/high-stuff.35/
+        https://www.28dayslater.co.uk/forum/industrial-sites.6/
+        https://www.28dayslater.co.uk/forum/leisure-sites.7/
+        https://www.28dayslater.co.uk/forum/residential-sites.92/
+        https://www.28dayslater.co.uk/forum/military-sites.5/
+        https://www.28dayslater.co.uk/forum/mines-and-quarries.95/
+        https://www.28dayslater.co.uk/forum/roc-posts.50/
+        https://www.28dayslater.co.uk/forum/restored-roc-posts.82/
+        https://www.28dayslater.co.uk/forum/theatres-and-cinemas.78/
+        https://www.28dayslater.co.uk/forum/uk-draining-forum.94/
+        https://www.28dayslater.co.uk/forum/underground-sites.29/
+        https://www.28dayslater.co.uk/forum/european-and-international-sites.46/
+        https://www.28dayslater.co.uk/forum/other-sites.8/
+        https://www.28dayslater.co.uk/forum/leads-rumours-and-news.57/
+        https://www.28dayslater.co.uk/forum/photo-threads.158/
+
+        https://www.28dayslater.co.uk/forum/diehardlove.122/
+        https://www.28dayslater.co.uk/forum/downfallen.121/
+        https://www.28dayslater.co.uk/forum/solomon.123/
+
+    """
+
+    get_forum_section("https://www.28dayslater.co.uk/forum/industrial-sites.6/?order=post_date&direction=desc")
+
+
     return
 
 
@@ -80,10 +113,9 @@ def get_forum_section(url: str, page: int = 1) -> None:
 
 
     for i in range(page+1, max_pages + 1):
-        print(f"crawling page {i} of {max_pages}")
         xx = url_parse._replace(path=f"{url_parse.path}page-{i}").geturl()
         get = get_forum_section_page(xx)
-        print(get)
+        print(f"[{get.status_code}]crawling page {i} of {max_pages}")
 
         if get.error_cnt == get.thread_cnt:
             print(f"breaking on page {i} (possibly no newer threads)")
@@ -92,7 +124,7 @@ def get_forum_section(url: str, page: int = 1) -> None:
 
 
 def get_forum_section_page(url: str) -> int:
-    nt = namedtuple('gfsp', ['max_pages', 'error_cnt', 'thread_cnt'])
+    nt = namedtuple('gfsp', ["max_pages", "error_cnt", "thread_cnt", "status_code"])
 
     #https://www.28dayslater.co.uk/forum/industrial-sites.6/page-2?order=post_date&direction=desc
     page = requests.get(url)
@@ -102,19 +134,22 @@ def get_forum_section_page(url: str) -> int:
 
     max_pages = soup.select("ul.pageNav-main > li.pageNav-page:nth-last-of-type(1) > a")[0].text
     err_count = 0
+    cur.execute("BEGIN")
 
     for x in list_of_threads:
         title = str(x.select_one(".structItem-title").get_text().strip())
         thread_url = x.select("a")[0].get("href")
         thread_url_abs = urljoin(url, thread_url)
-
+        thread_date = x.select_one("time").get("datetime")
+                
         try:
-            cur.execute("INSERT INTO refs VALUES (NULL, ?, ?, NULL, ?, NULL, NULL)", (thread_url_abs, title, crawl_date))
+            cur.execute("INSERT INTO refs VALUES (NULL, ?, ?, NULL, ?, NULL, ?, NULL)", (thread_url_abs, title, crawl_date, thread_date))
         except Exception:
             #find out the real exception type later
             err_count = err_count+1
+    cur.execute("COMMIT")
 
-    return nt(int(max_pages), err_count, len(list_of_threads))
+    return nt(int(max_pages), err_count, len(list_of_threads), page.status_code)
 
 
 def getThread(url:str) -> None:
