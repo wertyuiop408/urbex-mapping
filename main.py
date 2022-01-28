67ea4b69fd3a0b62d72d47db6e3c29992d8ffa5b
wertyuiop408 <wertyuiop408@users.noreply.github.com>
2022-01-28 11:30:54 +0000

    Show miles to nearby locations when adding places

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import argparse
 from datetime import datetime, timezone
+from math import radians, cos, sin, asin, sqrt
 
 from db import db
 import xxviii_dayslater as d2l
@@ -49,17 +50,37 @@ def add_place(args):
     dt = datetime.now(timezone.utc).isoformat(timespec="seconds")
 
     rng = 0.05
-    db.get_cur().execute("SELECT row_id, name FROM places WHERE long > ? AND long < ? AND lat > ? AND lat < ?", [lng-rng, lng+rng, lat-rng, lat+rng])
+    db.get_cur().execute("SELECT row_id, name, long, lat FROM places WHERE long > ? AND long < ? AND lat > ? AND lat < ?", [lng-rng, lng+rng, lat-rng, lat+rng])
     nearby = db.get_cur().fetchall()
     if len(nearby) != 0:
         print(f"{len(nearby)} nearby")
-        [print(f"* [{i['row_id']}] {i['name']}") for i in nearby]
-
+        for i in nearby:
+            print(f"[{i['row_id']}] ({haversine(i['long'], i['lat'], lng, lat)}) miles: {i['name']}")
 
     ins = db.get_cur().execute("INSERT INTO places(date_inserted, name, lat, long) VALUES (?, ?, ?, ?)", [dt, name, lat, lng]).rowcount
     db.get_cur().execute("SELECT row_id FROM places WHERE name = ? and lat = ? and long = ?", [name, lat, lng])
     res = db.get_cur().fetchone()
     print(f"Inserted {name} as ID {res['row_id']}")
+    return
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    # Radius of earth in kilometers is 6371
+    km = 6371 * c
+    mi = 3959 * c
+    return float(f"{mi:.2f}")
+
 
 if __name__ == "__main__":
     main()
