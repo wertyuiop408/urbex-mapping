af69ed2f56a0685ea6aef8649d98fb231c588540
wertyuiop408 <wertyuiop408@users.noreply.github.com>
2022-02-08 23:49:35 +0000

    Added command line option to change query

--- query.py
+++ query.py
@@ -9,6 +9,7 @@ def main() -> None:
 
     db.connect()
     parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument("-q", type=int, default=1)
     parser.add_argument("filter", nargs="?")
     parser.add_argument("--min", type=int, default=0)
     args = parser.parse_args()
@@ -18,8 +19,11 @@ def main() -> None:
     if args.filter:
         filt = f"and lower(title) like '{args.filter}'"
 
+    query_list = list()
+
     #find all entries that don't have a PLACE ID and find the most common words (uses split to remove a lot of the unwanted)
-    query = f"""
+    #q 0
+    query_list.append(f"""
     WITH split(name, rest, lev) AS (
         SELECT
             NULL AS name,
@@ -42,11 +46,12 @@ def main() -> None:
     GROUP BY name
     ORDER BY tot DESC
     LIMIT 200
-    """
+    """)
 
 
     #select all reports that have no place ID, show the most common words
-    query2 = f"""
+    #q 1
+    query_list.append(f"""
     WITH split(name, rest, lev) AS (
         SELECT
             NULL AS name, 
@@ -76,10 +81,11 @@ def main() -> None:
     SELECT name, count(name) AS tot FROM split WHERE name <> ''
     GROUP BY name HAVING tot > ?
     ORDER BY tot ASC
-    """
+    """)
 
     #show the most popular beginning letters of a word from reports, e.g. "sheff"
-    query3 = """
+    #q 2
+    query_list.append("""
     SELECT
         SUBSTR(
             TRIM(
@@ -93,10 +99,29 @@ def main() -> None:
         count(*) as cnt 
         FROM refs WHERE place_id IS 0
         AND title LIKE 'Report -%' AND foo <> ''
-        GROUP BY foo
+        GROUP BY foo HAVING cnt > ?
         ORDER By cnt DESC
-    """
+    """)
 
+    #select the most popular first word after x, e.g after report - 
+    #q3
+    query_list.append("""
+    SELECT
+        LOWER(
+            TRIM(
+                SUBSTR(title, 14,
+                    INSTR(
+                        SUBSTR(title, 14) /*14 is the length of 'report -_raf '*/
+                    , ' ')
+                ), ' ,-' /*remove unwanted chars from edges of the word*/
+            )
+        ) AS name,
+        COUNT(*) as tot
+    FROM refs
+    WHERE title like 'report -_raf %' and place_id = 0 and name <> ''
+    GROUP BY name having tot > ?
+    ORDER BY tot ASC
+    """)
 
     #replacing all of these in SQL is not pretty
     word_list = [
@@ -136,9 +161,11 @@ def main() -> None:
         "\nabandoned"
     ]
 
-    start = time.perf_counter()
 
-    for x in db.get_cur().execute(query2, [args.min]):
+    start = time.perf_counter()
+    xx = (query_list[args.q], [args.min])
+
+    for x in db.get_cur().execute(*xx):
 
         name = re.sub(r"\s+", "", x["name"])        
         if name not in word_list:
