b3c7709f79b4d39f7e10ba073560fbdf12d7422e
wertyuiop408 <wertyuiop408@users.noreply.github.com>
2023-03-26 19:10:29 +0100

    Added basic crawler for wordpress sites

--- src/urbex/wordpress.py
+++ src/urbex/wordpress.py
@@ -1,154 +1,42 @@
-import json
-import math
+import asyncio
+from aiohttp.client import ClientSession
 from datetime import datetime, timezone
-from urllib.parse import urlsplit, urljoin
 
-import requests
-from bs4 import BeautifulSoup
-import tomlkit
-import urllib3
-
-from db import db
+from db_tables import refs
+from spider import *
 
 
-#disable warnings in requests about SSL certs being invalid
-urllib3.disable_warnings()
+class wordpress(spider):
+    def __init__(self, url_: str, sess: ClientSession) -> None:
+        self.sess = sess  # type: ClientSession
+        self.crawl_times = dict()  # type: ignore
 
+        self.base_url = url_.strip(" ").rstrip("/") + "/"
 
-class wordpress:
-    def __init__(self, cfg, index = 0) -> None:
-        """
-        https://developer.wordpress.org/rest-api/reference/posts/#list-posts
+    async def parse(self, res, *cb1, **cb2):
+        crawl_date = datetime.now(timezone.utc).isoformat(timespec="seconds")
 
-        The WP Rest API sends the total count(found_posts) property from WP_Query. in a header called X-WP-Total.
-            X-WP-Total:71
-            X-WP-TotalPages:8
-
-
-        """
-        self.cfg = cfg
-        self.index = index
-        return
-
-
-    def crawl(self):
-        fp = self.pagination()
-        if fp[0] == None:
-            print("    Not a valid wordpress site")
+        # check it's valid
+        header_wptotal = res.headers.get("X-WP-Total")
+        if not header_wptotal:
             return
-
-
-        for page in range(1, math.ceil(fp[1]/100)+1):
-            pagi = self.pagination(page, 100)
-            if pagi[0] == None:
-                return
-
-        write_time = datetime.now().isoformat(timespec="seconds")
-        self.cfg["lc"] = write_time
-        self.write_config()
-        return
-
-
-    def pagination(self, page=1, per_page=10):
-        _url = urljoin(self.cfg["url"], f"wp-json/wp/v2/posts?per_page={per_page}&page={page}")
-
-        if self.cfg.get("lc") != None:
-            _url += f"&after={self.cfg['lc']}"
-
-        fp = requests.get(_url, verify=False)
-        if page == 1:
-            print(f"[{fp.status_code}] "+ urljoin(self.cfg["url"], "/"))
 
         try:
-            #if the header doesn;t exist, or it's not json.
-            total = int(fp.headers["X-WP-Total"])
-            content = json.loads(fp.content)
+            content = await res.json()
         except Exception:
-            return (None, None)
-
-        self.save_pagination(content)
-        count = len(content)
-        return (count, total)
-
-
-    def save_pagination(self, content) -> None:
-        sql_stmnt = """INSERT OR IGNORE INTO refs(url, title, date_inserted, date_post) 
-            SELECT ?, ?, ?, ? WHERE NOT EXISTS (SELECT 1 FROM refs WHERE url = ?1)"""
-
-        crawl_date = datetime.now(timezone.utc).isoformat(timespec="seconds")
-        arr = list()
-
-        for item in content:
-            arr.append([
-                item["link"],
-                item["title"]["rendered"],
-                crawl_date,
-                item["date"]
-            ])
-
-        ins = db.get_cur().executemany(sql_stmnt, arr).rowcount
-        print(f"    inserted {ins} records")
-
-        return
-
-
-    def insert(self, url):
-        """
-        Insert a single reference URL
-        """
-        db.get_cur().execute("SELECT row_id FROM refs WHERE url = ?", [url])
-        res = db.get_cur().fetchone()
-        
-        if res is not None:
-            print("already exists")
             return
 
-        page = requests.get(url, verify=False)
-        soup = BeautifulSoup(page.content, "html.parser")
-        title = soup.find("meta", property="og:title")["content"]
-        _url = soup.find("meta", property="og:url")["content"]
-        pt = soup.find("meta", property="article:published_time")["content"]
+        ret_list = list()
+        for row in content:
+            if not isinstance(row, dict):
+                return
 
-        sql_stmnt = """INSERT OR IGNORE INTO refs(url, title, date_inserted, date_post) 
-            SELECT ?, ?, ?, ? WHERE NOT EXISTS (SELECT 1 FROM refs WHERE url = ?1)"""
-
-        crawl_date = datetime.now(timezone.utc).isoformat(timespec="seconds")
-        db.get_cur().execute(sql_stmnt, [_url, title, crawl_date, pt])
-
-        return
-
-
-    def write_config(self) -> None:
-        """
-        Saves the changes to the configuration to file.
-        The changes are the latest times of the crawl for each subreddit
-        """
-        with open("config.cfg", mode="r+t", encoding="utf-8") as fp:
-            cfg = tomlkit.load(fp)
-            fp.seek(0)
-
-            # update the crawlers config and write to file
-            cfg["crawler"]["wordpress"][self.index].update(self.cfg)
-            fp.write(tomlkit.dumps(cfg))
-        return
-
-      
-if __name__ == "__main__":
-    """
-    #not really needed with the config now
-    parser = argparse.ArgumentParser(allow_abbrev=False)
-    parser.add_argument('site')
-    args = parser.parse_args()
-    """
-    db.connect()
-
-    with open("config.cfg", mode="rt", encoding="utf-8") as f:
-        conf = tomlkit.load(f)
-
-    if conf.get("crawler", {}).get("wordpress") == None:
-        print("No wordpress entry found")
-        exit()
-
-    for index, wp in enumerate(conf["crawler"]["wordpress"]):
-        x = wordpress(wp, index)
-        x.crawl()
+            data = refs(
+                title=row["title"]["rendered"],
+                url=row["link"],
+                date_post=row["date"],
+                date_inserted=crawl_date,
+            )
+            ret_list.append(data.__dict__)
+        self.save_to_db(ret_list)
+        return ret_list
---
+++ tests/test_wordpress.py
@@ -0,0 +1,145 @@
+import asyncio
+import builtins
+from functools import partial
+from unittest.mock import patch, mock_open
+
+from wordpress import wordpress
+from db_base import session_factory
+from db_tables import refs
+
+import pytest
+import aiohttp
+from aioresponses import aioresponses
+
+
+URL_ = "https://example.com"
+SECT_MAX_PAGES = 215
+SECT_DATA_LEN = 10
+
+BASE_URL = "https://www.whateversleft.co.uk/"
+POST_URL = "https://www.whateversleft.co.uk/wp-json/wp/v2/posts?per_page=10&page=1"
+POSTS_DATA = [
+    {
+        "title": "Bank of England House, Bristol",
+        "url": "https://www.whateversleft.co.uk/other/bank-of-england-house-bristol/",
+        "date_post": "2022-09-20T19:08:06",
+    },
+    {
+        "title": "Springfield Hospital, Tooting",
+        "url": "https://www.whateversleft.co.uk/asylums/springfield-hospital-tooting/",
+        "date_post": "2021-02-12T17:29:24",
+    },
+    {
+        "title": "Brislington War Room, Region 7, Bristol",
+        "url": "https://www.whateversleft.co.uk/underground/brislington-war-room-region-7-bristol/",
+        "date_post": "2020-10-05T13:03:47",
+    },
+    {
+        "title": "Bomb Proof Records Room, Bristol",
+        "url": "https://www.whateversleft.co.uk/underground/bomb-proof-records-room-bristol/",
+        "date_post": "2020-06-29T11:00:31",
+    },
+    {
+        "title": "Shedload, Bristol",
+        "url": "https://www.whateversleft.co.uk/industrial/shedload-bristol/",
+        "date_post": "2020-04-04T13:40:25",
+    },
+    {
+        "title": "Whitchurch Hospital, Cardiff",
+        "url": "https://www.whateversleft.co.uk/asylums/whitchurch-hospital-cardiff/",
+        "date_post": "2019-03-16T20:29:03",
+    },
+    {
+        "title": "Bristol Royal Infirmary &#8211; Old Building, Bristol",
+        "url": "https://www.whateversleft.co.uk/hospitals/bristol-royal-infirmary-old-building-bristol/",
+        "date_post": "2018-10-01T14:05:01",
+    },
+    {
+        "title": "Sunnyside Royal Hospital, Montrose",
+        "url": "https://www.whateversleft.co.uk/asylums/sunnyside-royal-hospital-montrose-2/",
+        "date_post": "2018-04-22T20:36:56",
+    },
+    {
+        "title": "Plaza Cinema, Port Talbot",
+        "url": "https://www.whateversleft.co.uk/leisure/plaza-cinema-port-talbot/",
+        "date_post": "2018-01-28T18:20:41",
+    },
+    {
+        "title": "Robert Fletcher &#038; Son Ltd &#8211; Greenfield, Oldham",
+        "url": "https://www.whateversleft.co.uk/industrial/robert-fletcher-son-ltd-greenfield-oldham/",
+        "date_post": "2018-01-23T16:45:04",
+    },
+]
+
+
+@pytest.fixture
+def mock():
+    with aioresponses() as m:
+        yield m
+
+
+async def test_200_posts(mock):
+    with open("tests/wp_posts.json", "r") as fp:
+        file_data = fp.read()
+
+    mock.get(POST_URL, status=200, body=file_data, headers={"X-WP-Total": "10"})
+    async with aiohttp.ClientSession() as session:
+        wp = wordpress(BASE_URL, session)
+        res, cb = await wp.get_url(POST_URL, partial(wp.parse, nxt=False))
+
+        assert len(cb) == 10
+
+        for x in zip(POSTS_DATA, cb):
+            assert x[0]["title"] == x[1]["title"]
+            assert x[0]["url"] == x[1]["url"]
+            assert x[0]["date_post"] == x[1]["date_post"]
+
+
+async def test_200_no_wpheader(mock):
+    with open("tests/wp_posts.json", "r") as fp:
+        file_data = fp.read()
+
+    mock.get(POST_URL, status=200, body=file_data)
+    async with aiohttp.ClientSession() as session:
+        wp = wordpress(BASE_URL, session)
+        res, cb = await wp.get_url(POST_URL, partial(wp.parse, nxt=False))
+        assert cb == None
+
+
+async def test_empty_page(mock):
+    mock.get(POST_URL, status=200, body="<html>", headers={"X-WP-Total": "10"})
+    async with aiohttp.ClientSession() as session:
+        wp = wordpress(BASE_URL, session)
+        res, cb = await wp.get_url(POST_URL, partial(wp.parse, nxt=False))
+        assert cb == None
+
+
+async def test_error_page(mock):
+    with open("tests/wp_posts_error.json", "r") as fp:
+        file_data = fp.read()
+
+    mock.get(POST_URL, status=400, body=file_data, headers={"X-WP-Total": "10"})
+    async with aiohttp.ClientSession() as session:
+        wp = wordpress(BASE_URL, session)
+        res, cb = await wp.get_url(POST_URL, partial(wp.parse, nxt=False))
+        assert cb == None
+
+
+async def test_db_section(mock):
+    with open("tests/wp_posts.json", "r") as fp:
+        file_data = fp.read()
+
+    mock.get(POST_URL, status=200, body=file_data, headers={"X-WP-Total": "10"})
+    mock.get(POST_URL, status=200, body=file_data, headers={"X-WP-Total": "10"})
+    async with aiohttp.ClientSession() as session:
+        wp = wordpress(BASE_URL, session)
+        res, cb = await wp.get_url(POST_URL, partial(wp.parse, nxt=False))
+
+        db_sess = session_factory()
+        db_count = db_sess.query(refs).count()
+        assert db_count == 10
+
+        # check for duplicate entries
+        await wp.get_url(POST_URL, partial(wp.parse, nxt=False))
+        db_count = db_sess.query(refs).count()
+        assert db_count == 10
