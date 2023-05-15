6df3208793d15a242027dc7555bc619cfc202b8b
wertyuiop408 <wertyuiop408@users.noreply.github.com>
2023-05-15 09:39:34 +0100

    Added latest api/template

--- src/urbex/web.py
+++ src/urbex/web.py
@@ -19,9 +19,9 @@ async def index() -> Template:
     return Template(name="index.html")
 
 
-@get("/a/{name:str}")
-async def greeter(name: str) -> str:
-    return "Hello, " + name
+@get("/latest")
+async def latest() -> Template:
+    return Template(name="latest.html")
 
 
 @get("/bounds/{ne_lat: decimal}/{ne_lng: decimal}/{sw_lat: decimal}/{sw_lng: decimal}")
@@ -71,7 +71,7 @@ async def search(query_: str) -> str:
 
 # @app.route("/search/<query>")
 app = Litestar(
-    route_handlers=[index, greeter, get_bounds, search],
+    route_handlers=[index, latest, get_bounds, search],
     static_files_config=[StaticFilesConfig(directories=["static"], path="/static")],
     template_config=TemplateConfig(directory=".", engine=JinjaTemplateEngine),
 )
