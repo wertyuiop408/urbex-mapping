d3e472c6b15772bb723a98af4aaf3ce19f32fd56
wertyuiop408 <wertyuiop408@users.noreply.github.com>
2023-05-23 07:59:14 +0100

    Fast and simple search query using indexed table

--- src/urbex/web.py
+++ src/urbex/web.py
@@ -77,26 +77,19 @@ async def search(query_: str) -> list[dict[str, str | bool]]:
     start = time.time()
     # search the virtual table for the tag, and order it by the bm25 algorithm using 'rank'. Then grab the related place
     stmt = text(
-        # Result: 10 rows returned in 311ms (limit 15/10)
-        # Result: 20 rows returned in 559ms (limit 30/20)
-        # Result: Result: 791 rows returned in 14212ms (no limit)
-        """SELECT * FROM (
-            SELECT places.row_id as pid,
-                places.long,
-                places.lat,
-                places.name,
-                tags_ft.tag,
-                rank 
-                FROM tags_ft
-            LEFT JOIN tag_rel ON tag_rel.tag_id=tags_ft.rowid
-            LEFT JOIN places ON places.row_id=tag_rel.place_id
-            WHERE tags_ft.tag MATCH :query
-
-            ORDER BY rank
-            LIMIT 15)
-        GROUP BY pid
-        ORDER BY rank
-        LIMIT 10"""
+        """
+        SELECT places.row_id,
+            places.long,
+            places.lat,
+            places.name,
+            tags_ft.tag FROM tag_rel
+        JOIN places ON places.row_id=tag_rel.place_id
+        JOIN tags_ft ON tags_ft.rowid=tag_rel.tag_id
+        WHERE tags_ft.tag match :query
+        GROUP BY place_id
+        ORDER BY tags_ft.rank
+        LIMIT 15
+        """
     )
 
     res = db.execute(stmt, {"query": f"{query_}*"}).all()
